53110d068470ebe484fbcae2b58b13013ed20c72
Gabe <Carrollgbusiness@gmail.com>
2026-04-16 15:19:19 -0500

    First Commit

---
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -0,0 +1,57 @@
+package com.pluralsight;
+
+public class CellPhone {
+ private int sNumber;
+ private String model;
+ private String  carrier ;
+ private String pNumber ;
+ private String owner;
+
+ public CellPhone() {
+     this.sNumber = 0;
+     this.model =" ";
+     this.carrier =" ";
+     this.pNumber =" ";
+     this.owner =" ";
+ }
+
+    public int getsNumber() {
+        return sNumber;
+    }
+
+    public void setsNumber(int sNumber) {
+        this.sNumber = sNumber;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public String getpNumber() {
+        return pNumber;
+    }
+
+    public void setpNumber(String pNumber) {
+        this.pNumber = pNumber;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+}
---
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -0,0 +1,37 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class CellPhoneApplication {
+    public static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        CellPhone thePhone = new CellPhone();
+
+        System.out.print("whats the serial number? ");
+        int sNum = input.nextInt();
+        thePhone.setsNumber(sNum);
+        // eats line b/c string to int
+        input.nextLine();
+
+        System.out.print("whats the model? ");
+        String model = input.nextLine();
+        thePhone.setModel(model);
+
+        System.out.print("whats the Carrier? ");
+        String carrier = input.nextLine();
+        thePhone.setCarrier(carrier);
+
+        System.out.print("whats the Phone Number? ");
+        String pNumber = input.nextLine();
+        thePhone.setpNumber(pNumber);
+
+        System.out.print("who's the Owner? ");
+        String owner = input.nextLine();
+        thePhone.setOwner(owner);
+
+
+
+
+
+    }
+}
